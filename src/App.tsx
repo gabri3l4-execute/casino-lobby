4e67c5282ce28d64c2f79c3ba5efe1e3b604d43c
Gabri3l4.exe <gabri3l4.exe@gmail.com>
2025-11-15 23:48:01 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/crown.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo crown" alt="React logo" />
+      </div>
+      <h1>Casino lobby</h1>
+    </>
+  );
+}
+
+export default App;
